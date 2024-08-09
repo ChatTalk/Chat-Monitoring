a2f9e2fa9b35662620ed5a2a0302a57707d29327
kimD0ngjun <chickentasty0112@gmail.com>
2024-08-10 00:59:49 +0900

    feat: promQL 관련 고려 필요

--- src/main/java/com/example/monitor/service/PrometheusService.java
+++ src/main/java/com/example/monitor/service/PrometheusService.java
@@ -5,6 +5,9 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+
 @Service
 public class PrometheusService {
 
@@ -16,7 +19,10 @@ public class PrometheusService {
     }
 
     public Mono<String> fetchMetrics() {
+//        String query = URLEncoder.encode("sum(http_requests_count{endpoint=\"/api/user\"}) by (endpoint)", StandardCharsets.UTF_8);
+
         return webClient.get()
+//                .uri("/api/v1/query?query=" + query)
                 .uri("/api/v1/query?query=up") // 원하는 쿼리(PromQL)로 수정 가능
                 .retrieve()
                 .bodyToMono(String.class);
