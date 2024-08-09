aca1a7c49840df3ef324ae9db0dcd0ac8c2a6c61
kimD0ngjun <chickentasty0112@gmail.com>
2024-08-09 17:52:54 +0900

    refactor: 의존성 어노테이션 추가

--- src/main/java/com/example/monitor/service/PrometheusService.java
+++ src/main/java/com/example/monitor/service/PrometheusService.java
@@ -1,5 +1,6 @@
 package com.example.monitor.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
@@ -9,6 +10,7 @@ public class PrometheusService {
 
     private final WebClient webClient;
 
+    @Autowired
     public PrometheusService(WebClient.Builder webClientBuilder) {
         this.webClient = webClientBuilder.build();
     }
