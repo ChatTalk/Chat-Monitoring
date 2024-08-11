153824376f999cf43284da06d00ce5abd320a4c3
kimD0ngjun <chickentasty0112@gmail.com>
2024-08-11 21:56:50 +0900

    feat: promQL 별 컨트롤러 메소드 데이터 수집

--- src/main/java/com/example/monitor/service/PrometheusService.java
+++ src/main/java/com/example/monitor/service/PrometheusService.java
@@ -6,10 +6,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class PrometheusService {
@@ -21,37 +24,44 @@ public class PrometheusService {
         this.webClient = webClientBuilder.build();
     }
 
-    public Mono<String> fetchMetrics() {
-//        String query = URLEncoder.encode("sum(http_requests_count{endpoint=\"/api/user\"}) by (endpoint)", StandardCharsets.UTF_8);
+    public Mono<List<String>> fetchMetrics() {
+        List<String> queryParams = new ArrayList<>();
+        queryParams.add("book_get_count_total[5h]");
+        queryParams.add("books_get_count_total[5h]");
+        queryParams.add("loan_create_count_total[5h]");
+        queryParams.add("loan_update_count_total[5h]");
 
-//        return webClient.get()
-////                .uri("/api/v1/query?query=" + query)
-//                .uri("/api/v1/query?query=up") // 원하는 쿼리(PromQL)로 수정 가능
-//                .retrieve()
-//                .bodyToMono(String.class);
-        String query = "rate(book_get_count_total[5h])";
+        // 여기서 각각의 것들 객체화해서
 
-        return webClient.get()
-                .uri(uriBuilder -> uriBuilder
-                        .path("/api/v1/query")
-                        .queryParam("query", query)
-                        .build())
-                .retrieve()
-                .onStatus(
-                        HttpStatusCode::isError,
-                        response -> response.bodyToMono(String.class).flatMap(errorBody -> Mono.error(new RuntimeException("Error response: " + errorBody))))
-                .bodyToMono(String.class)
-                .doOnNext(responseBody -> {
-                    // JSON 포맷팅
-                    try {
-                        ObjectMapper mapper = new ObjectMapper();
-                        Object json = mapper.readValue(responseBody, Object.class);
-                        ObjectWriter writer = mapper.writerWithDefaultPrettyPrinter();
-                        String prettyJson = writer.writeValueAsString(json);
-                        System.out.println(prettyJson);
-                    } catch (Exception e) {
-                        e.printStackTrace();
-                    }
-                });
+        return Flux.fromIterable(queryParams)
+                .flatMap(queryParam -> {
+                    String query = "sum(rate(" + queryParam + ")) by (application, class, exception, instance, job, method, result)";
+
+                    return webClient.get()
+                            .uri(uriBuilder -> uriBuilder
+                                    .path("/api/v1/query")
+                                    .queryParam("query", query)
+                                    .build())
+                            .retrieve()
+                            .onStatus(
+                                    HttpStatusCode::isError,
+                                    response -> response.bodyToMono(String.class)
+                                            .flatMap(errorBody -> Mono.error(new RuntimeException("Error response: " + errorBody)))
+                            )
+                            .bodyToMono(String.class)
+                            .doOnNext(responseBody -> {
+                                // JSON 포맷팅
+                                try {
+                                    ObjectMapper mapper = new ObjectMapper();
+                                    Object json = mapper.readValue(responseBody, Object.class);
+                                    ObjectWriter writer = mapper.writerWithDefaultPrettyPrinter();
+                                    String prettyJson = writer.writeValueAsString(json);
+                                    System.out.println(prettyJson);
+                                } catch (Exception e) {
+                                    e.printStackTrace();
+                                }
+                            });
+                })
+                .collectList();
     }
 }
