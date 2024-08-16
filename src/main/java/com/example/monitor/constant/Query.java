2135eccd04902560cc1427a73ca76b31631fb575
kimD0ngjun <chickentasty0112@gmail.com>
2024-08-17 03:15:40 +0900

    feat: 쿼리 설명 추가

--- src/main/java/com/example/monitor/constant/Query.java
+++ src/main/java/com/example/monitor/constant/Query.java
@@ -6,8 +6,8 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public enum Query {
-    BOOK_GET_COUNT_TOTAL("book_get_count_total"),
-    BOOK_GET_TIMED_SECONDS_SUM("book_get_timed_seconds_sum"),
+    BOOK_GET_COUNT_TOTAL("book_get_count_total"), // 메소드 호출 횟수 시간당 비율
+    BOOK_GET_TIMED_SECONDS_SUM("book_get_timed_seconds_sum"), // 메소드 실행 시간 평균 비율
     BOOKS_GET_COUNT_TOTAL("books_get_count_total"),
     BOOKS_GET_TIMED_SECONDS_SUM("books_get_timed_seconds_sum");
 
