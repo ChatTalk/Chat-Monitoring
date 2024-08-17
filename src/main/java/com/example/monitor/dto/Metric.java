d35823bb2e7c37f758196bf4d057b319ab89b798
kimD0ngjun <chickentasty0112@gmail.com>
2024-08-17 15:00:21 +0900

    feat: 메트릭 데이터 dto 생성

--- src/main/java/com/example/monitor/dto/Metric.java
+++ src/main/java/com/example/monitor/dto/Metric.java
@@ -1,4 +1,29 @@
 package com.example.monitor.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
 public class Metric {
+    private String instance;
+    private String application;
+    private String className;
+    private String methodName;
+    private MetricType metricType; // RATE or AVERAGE_TIME
+    private ResultType resultType; // SUCCESS or FAILURE
+    private String exception; // 결과가 FAILURE 일 경우, 반환한 예외 확인(SUCCESS 일 경우에는 null)
+    private Double value; // 메트릭 타입에 따른 값
+
+    public enum MetricType {
+        CALL_COUNT,          // 시간당 호출 빈도수
+        AVERAGE_TIME   // 평균 응답 시간
+    }
+
+    public enum ResultType {
+        SUCCESS,
+        FAILURE
+    }
 }
