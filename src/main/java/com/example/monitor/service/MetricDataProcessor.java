9f1419013f28269ace790be1b2b784b9aa1c2b97
kimD0ngjun <chickentasty0112@gmail.com>
2024-08-17 20:36:48 +0900

    refactor: 임시 메트릭 DTO 확인용 로그 추가

--- src/main/java/com/example/monitor/service/MetricDataProcessor.java
+++ src/main/java/com/example/monitor/service/MetricDataProcessor.java
@@ -14,30 +14,27 @@ public class MetricDataProcessor {
                 .subscribe(response -> {
                     // JSON 파싱 및 데이터 처리 로직 추가
                     response.stream()
-                            .map(metric -> {
-                                // 각 Metric 객체의 필드를 추출하고 필요한 변환을 수행합니다.
-                                return String.format(
-                                        "Instance: %s, " +
-                                                "Application: %s, " +
-                                                "ClassName: %s, " +
-                                                "MethodName: %s, " +
-                                                "MetricType: %s, " +
-                                                "ResultType: %s, " +
-                                                "Exception: %s, " +
-                                                "Value: %f" +
-                                                "TimeStamp: %s"
-                                        ,
-                                        metric.getInstance(),
-                                        metric.getApplication(),
-                                        metric.getClassName(),
-                                        metric.getMethodName(),
-                                        metric.getMetricType(),
-                                        metric.getResultType(),
-                                        metric.getException(),
-                                        metric.getValue(),
-                                        metric.getTime()
-                                );
-                            })
+                            .map(metric -> String.format(
+                                    "Instance: %s, " +
+                                            "Application: %s, " +
+                                            "ClassName: %s, " +
+                                            "MethodName: %s, " +
+                                            "MetricType: %s, " +
+                                            "ResultType: %s, " +
+                                            "Exception: %s, " +
+                                            "Value: %f" +
+                                            "TimeStamp: %s"
+                                    ,
+                                    metric.getInstance(),
+                                    metric.getApplication(),
+                                    metric.getClassName(),
+                                    metric.getMethodName(),
+                                    metric.getMetricType(),
+                                    metric.getResultType(),
+                                    metric.getException(),
+                                    metric.getValue(),
+                                    metric.getTime()
+                            ))
                             .forEach(System.out::println); // 변환된 문자열을 출력
                 });
     }
