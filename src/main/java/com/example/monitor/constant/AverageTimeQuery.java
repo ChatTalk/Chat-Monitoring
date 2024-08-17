e6cc2ad712fa10b4bb98641abb0c4dc4eb5b5714
kimD0ngjun <chickentasty0112@gmail.com>
2024-08-17 13:42:59 +0900

    fix: 쿼리 상위 추상체 구현

--- src/main/java/com/example/monitor/constant/AverageTimeQuery.java
+++ src/main/java/com/example/monitor/constant/AverageTimeQuery.java
@@ -5,12 +5,13 @@ import lombok.RequiredArgsConstructor;
 
 @Getter
 @RequiredArgsConstructor
-public enum AverageTimeQuery {
+public enum AverageTimeQuery implements Query {
     BOOK_GET_TIMED_SECONDS_SUM("book_get_timed_seconds_sum"), // 메소드 실행 시간 평균 비율
     BOOKS_GET_TIMED_SECONDS_SUM("books_get_timed_seconds_sum");
 
     private final String queryParam;
 
+    @Override
     public String getPrometheusQuery() {
         return "sum(rate(" + queryParam + "[30s])) by (application, class, exception, instance, job, method, result)";
     }
--- src/main/java/com/example/monitor/constant/CallCountQuery.java
+++ src/main/java/com/example/monitor/constant/CallCountQuery.java
@@ -5,12 +5,13 @@ import lombok.RequiredArgsConstructor;
 
 @Getter
 @RequiredArgsConstructor
-public enum CallCountQuery {
+public enum CallCountQuery implements Query {
     BOOK_GET_COUNT_TOTAL("book_get_count_total"), // 메소드 호출 횟수 시간당 비율
     BOOKS_GET_COUNT_TOTAL("books_get_count_total");
 
     private final String queryParam;
 
+    @Override
     public String getPrometheusQuery() {
         return "sum(rate(" + queryParam + "[30s])) by (application, class, exception, instance, job, method, result)";
     }
