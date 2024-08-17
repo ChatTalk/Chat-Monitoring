bcd598eb7e0d8ce3ac95bc67487de04a43d02fa0
kimD0ngjun <chickentasty0112@gmail.com>
2024-08-17 21:59:29 +0900

    feat: 로그 핸들러 출력 로직 수정

--- src/main/java/com/example/monitor/constant/StandardValue.java
+++ src/main/java/com/example/monitor/constant/StandardValue.java
@@ -1,6 +1,8 @@
 package com.example.monitor.constant;
 
+/**
+ * 향후, 저장이나 경고 핸들러에서 기준값으로 삼기 위한 상수 모음 클래스
+ */
 public class StandardValue {
-    public final static double SUCCESS_STANDARD = 0;
-    public final static double FAILURE_STANDARD = 0;
+    public final static double WARN_STANDARD = 0;
 }
--- src/main/java/com/example/monitor/handler/LogHandler.java
+++ src/main/java/com/example/monitor/handler/LogHandler.java
@@ -1,6 +1,5 @@
 package com.example.monitor.handler;
 
-import com.example.monitor.constant.StandardValue;
 import com.example.monitor.dto.Metric;
 import lombok.extern.slf4j.Slf4j;
 
@@ -31,17 +30,20 @@ public class LogHandler extends Handler {
                 metric.getTime()
         );
 
-        if (metric.getResultType() == Metric.ResultType.SUCCESS) {
-            if (metric.getValue() >= StandardValue.SUCCESS_STANDARD) {
+        // 메트릭 타입 및 결과값에 따른 로그 출력
+        // 호출수 기준은 결과 타입에 따라서
+        if (metric.getMetricType() == Metric.MetricType.CALL_COUNT) {
+            if (metric.getResultType().equals(Metric.ResultType.SUCCESS)) {
+                log.info(output);
+            } else if (metric.getResultType().equals(Metric.ResultType.FAILURE)) {
+                log.warn(output);
+            }
+        // 빈도수 기준은 예외 발생 여부에 따라서
+        } else if (metric.getMetricType() == Metric.MetricType.AVERAGE_TIME) {
+            if (metric.getException().equals("none")) {
                 log.info(output);
             } else {
                 log.warn(output);
-            }
-        } else if (metric.getResultType() == Metric.ResultType.FAILURE) {
-            if (metric.getValue() >= StandardValue.FAILURE_STANDARD) {
-                log.warn(output);
-            } else {
-                log.error(output);
             }
         }
     }
