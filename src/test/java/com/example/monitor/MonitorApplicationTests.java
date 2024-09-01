628161956a7a574b10a157455b9b20cee759a1bd
kimD0ngjun <chickentasty0112@gmail.com>
2024-09-01 19:47:14 +0900

    feat: 모니터링 툴 통합을 위한 도커파일 작성

--- src/test/java/com/example/monitor/MonitorApplicationTests.java
+++ src/test/java/com/example/monitor/MonitorApplicationTests.java
@@ -3,7 +3,7 @@ package com.example.monitor;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = MonitorApplicationTests.class)
 class MonitorApplicationTests {
 
     @Test
